a78055e716c27bc492aa37b1c6d4ed8f506940ca
JSoun <149481722+jsoun@users.noreply.github.com>
2024-01-31 03:45:47 -0800

    Added files to project

---
+++ README.md
@@ -0,0 +1,1 @@
+After learning a few things about functions, operators, for loops, & methods. I tried my hands at making a simple rock, paper, scissor game!
---
+++ index.js
@@ -0,0 +1,48 @@
+const player = document.querySelector("#player");
+const computer = document.querySelector("#computer");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
+const message = document.querySelector("#message");
+
+const signs = ["🪨", "📄", "✂️"];
+
+// game logic, updates message according to outcome
+function gameSystem() {
+  if (
+    (player.textContent === signs[0] && computer.textContent === signs[1]) ||
+    (player.textContent === signs[1] && computer.textContent === signs[2]) ||
+    (player.textContent === signs[2] && computer.textContent === signs[0])
+  ) {
+    message.textContent = "You lose this round!";
+  } else if (player.textContent === computer.textContent) {
+    message.textContent = "It's a tie!";
+  } else {
+    message.textContent = "You win this round!";
+  }
+}
+
+// randomizes the computer signs
+function computerChoice() {
+  const randomSigns = Math.floor(Math.random() * signs.length);
+  computer.textContent = signs[randomSigns];
+}
+
+// click listeners that activate functions
+rock.addEventListener("click", function () {
+  player.textContent = signs[0];
+  computerChoice();
+  gameSystem();
+});
+
+paper.addEventListener("click", function () {
+  player.textContent = signs[1];
+  computerChoice();
+  gameSystem();
+});
+
+scissor.addEventListener("click", function () {
+  player.textContent = signs[2];
+  computerChoice();
+  gameSystem();
+});
